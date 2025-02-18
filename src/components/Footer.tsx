855304021a28d546249d075d4565d066bce0635c
Motiejuslimas <motiejus.slimas@gmail.com>
2025-02-18 12:45:39 +0200

    footer typo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <div className={"flex w-full items-center flex-col px-5"}>
             <div>
                 Designed in <b>MS Paint</b> while being sleep deprived.
-                Built using <b>React.js</b> and <b>Tailwind CSS</b>by yours truly.
+                Built using <b>React.js</b> and <b>Tailwind CSS</b> by yours truly.
             </div>
             <div>
                 © 2025 Motiejus Šlimas
