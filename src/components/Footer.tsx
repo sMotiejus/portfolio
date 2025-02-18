fc31836d2d0f917f36b4ab0e16cf52324433d33b
Motiejuslimas <motiejus.slimas@gmail.com>
2025-02-18 12:44:53 +0200

    footer padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
     return (
-        <div className={"flex w-full items-center flex-col"}>
-            <div>Designed in <b>MS Paint</b> while being sleep deprived. Built using <b>React.js</b> and <b>Tailwind
-                CSS</b> by yours
-                truly.
+        <div className={"flex w-full items-center flex-col px-5"}>
+            <div>
+                Designed in <b>MS Paint</b> while being sleep deprived.
+                Built using <b>React.js</b> and <b>Tailwind CSS</b>by yours truly.
             </div>
             <div>
                 © 2025 Motiejus Šlimas
